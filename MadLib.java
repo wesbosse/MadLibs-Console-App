1f930ccaf56d4528b2ca5c14db7f03947a91aa1b
wesbosse <wes.bosse@gmail.com>
2018-03-09 14:03:38 -0800

    working output

--- MadLib.java
+++ MadLib.java
@@ -3,8 +3,14 @@ import java.io.Console;
 public class MadLib {
 	public static void main(String[] args) {
 		Console console = System.console();
-		String name = console.readLine("What is your name? ");
-		console.printf("Hello %s!\n", name);
 
+		String name = console.readLine("Enter a name:  ");
+		String adjective = console.readLine("Enter an adjective:  ");
+		String noun = console.readLine("Enter a noun:  ");
+		String adverb = console.readLine("Enter an adverb:  ");
+		String verb = console.readLine("Enter a verb ending with -ing:  ");
+
+		console.printf("||----- Your Story -----||");
+		console.printf("%s is a %s %s. They are always %s %s.", name, adjective, noun, adverb, verb);
 	}
 }
