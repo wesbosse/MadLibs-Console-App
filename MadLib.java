b4393acb6a139ea1e2bbd1d951428cab1a82f793
wesbosse <wes.bosse@gmail.com>
2018-03-11 22:00:41 -0700

    playing with class structure/function declarations

--- MadLib.java
+++ MadLib.java
@@ -1,16 +1,41 @@
 import java.io.Console;
+import java.util.Random;
 
 public class MadLib {
+
+	// public static String adj(Console console) {
+	// 	String adjective = console.readLine("Enter an adjective:  ");
+	// 	return adjective;
+	// }
+	// public static String noun(Console console) {
+	// 	String noun = console.readLine("Enter a noun:  ");
+	// 	return noun;
+	// }
+	// public static String adv(Console console) {
+	// 	String adverb = console.readLine("Enter an adverb:  ");
+	// 	return adverb;
+	// }
+	// public static String verb(Console console) {
+	// 	String verb = console.readLine("Enter a verb ending with -ing:  ");
+	// 	return verb;
+	// }
+
+	public static String buildStory() {
+		Random randomNumber = new Random();
+		Console console = System.console();
+		int storySelector = randomNumber.nextInt(4);
+		console.printf("%s", storySelector);
+		console.printf("\n");
+		return "hello";
+	}
+
 	public static void main(String[] args) {
 		Console console = System.console();
-
 		String name = console.readLine("Enter a name:  ");
-		String adjective = console.readLine("Enter an adjective:  ");
-		String noun = console.readLine("Enter a noun:  ");
-		String adverb = console.readLine("Enter an adverb:  ");
-		String verb = console.readLine("Enter a verb ending with -ing:  ");
+	
 
-		console.printf("||----- Your Story -----||");
-		console.printf("%s is a %s %s. They are always %s %s.", name, adjective, noun, adverb, verb);
+		console.printf(buildStory());
+		console.printf("\n");
+		// console.printf("yo");
 	}
 }
